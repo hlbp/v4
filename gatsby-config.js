46f20841d19fe1b3950e710d156cc7c7cc4aaa1d
Helmer Barcos <helmerluis@hotmail.com>
2021-07-14 23:27:15 +0200

    adding demo on right path

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Helmer Barcos is a Full Stack Developer who specializes in building bridges between humans and machines.',
     siteUrl: 'https://barcos.co', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
+    image: '/demo.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@HelmerBarcos',
   },
   plugins: [
